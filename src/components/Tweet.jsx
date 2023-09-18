dd0308fe9b6da0116618a5518f3ac02e519f1708
MAH <108659794+Mahtabvariyani@users.noreply.github.com>
2023-09-18 16:44:40 +0200

    Add files via upload

---
+++ src/components/Tweet.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+function Tweet({ profileImage, name, handle, timestamp, message }) {
+  return (
+    <div className="tweet">
+      <img src={profileImage} className="profile" alt="profile" />
+
+      <div className="body">
+        <div className="top">
+          <span className="user">
+            <span className="name">{name}</span>
+            <span className="handle">@{handle}</span>
+          </span>
+
+          <span className="timestamp">{timestamp}</span>
+        </div>
+
+        <p className="message">{message}</p>
+
+        <div className="actions">
+          {/* Font Awesome icons */}
+          <i className="far fa-comment" data-testid="comment-icon"></i>
+          <i className="fas fa-retweet" data-testid="retweet-icon"></i>
+          <i className="far fa-heart" data-testid="heart-icon"></i>
+          <i className="fas fa-share" data-testid="share-icon"></i>
+        </div>
+      </div>
+
+      <i className="fas fa-ellipsis-h"></i>
+    </div>
+  );
+}
+
+export default Tweet;
